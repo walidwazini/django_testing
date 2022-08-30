ed0271bb409dd3c78c5798a98e5cd629fe465dc9
Walid Wazini <wazini25@gmail.com>
2022-08-30 16:08:31 +0800

    updates

---
+++ challenges/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+# * URL config
+urlpatterns = [
+    #! The order of path is IMPORTANT
+    path('<int:month>', views.monthly_challenge_by_number),
+    path('<str:month>', views.monthly_challenge)
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,32 @@
-from django.http import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+# * This is library, consist of key : value
+monthly_target = {
+    'jan': 'Eat more meat for entire month.',
+    'feb': 'Do sunning for atleast 2 hours daily.',
+    'mar': 'Drinks more plain water.',
+    'apr': 'Eat more fish.',
+    'may': 'Eat more meat for entire month.',
+    'jun': 'Do sunning for atleast 2 hours daily.',
+    'jul': 'Drinks more plain water.',
+    'aug': 'Eat more fish.',
+    'sep': 'Eat more meat for entire month.',
+    'oct': 'Do sunning for atleast 2 hours daily.',
+    'nov': 'Exercise 30minutes daily.',
+    'dec': 'Read more books.'
+}
 
-def index(request):
-    return HTTPResponse('This works.')
+
+def monthly_challenge_by_number(req, month):
+    return HttpResponse(month)
+
+
+def monthly_challenge(req, month):
+    try:
+        challenge_text = monthly_target[month]
+    except:
+        return HttpResponseNotFound('The value not availble.')
+    return HttpResponse(challenge_text)
